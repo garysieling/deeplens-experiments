279e0cc23994c840b1f1485ca23364b75b4db3a3
Gary Sieling <gary.sieling@gmail.com>
2018-05-18 17:24:59 -0400

    Update run_jenkins.sh

--- run_jenkins.sh
+++ run_jenkins.sh
@@ -3,6 +3,7 @@ docker build --tag garysieling/jenkins .
 
 docker run \
   -p 8080:8080 \
+  -p 50000:50000 \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v jenkins_home:/var/jenkins_home \
   --name jenkins \
