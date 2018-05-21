5956fa146a571c4f2137d782cc7f8acd830dc762
gary <gary@garysieling.com>
2018-05-20 21:57:32 -0400

    Fix build error

--- training/test.sh
+++ training/test.sh
@@ -1,2 +1,1 @@
-docker run --rm -e PASSWORD="pass" -v /home/admin:/home/jovyan/work --mount type=bind,source=/data,target=/data segaleran/op
-encv-jupyter python test.py
+docker run --rm -e PASSWORD="pass" -v /home/admin:/home/jovyan/work --mount type=bind,source=/data,target=/data segaleran/opencv-jupyter python test.py
