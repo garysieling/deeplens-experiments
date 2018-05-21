ccc3e7fbfca6d7060b288adff4bc45736aab9a8a
gary <gary@garysieling.com>
2018-05-20 22:07:02 -0400

    New try

--- training/test.sh
+++ training/test.sh
@@ -1,3 +1,4 @@
-CMD=docker run --rm -e PASSWORD="pass" -v `pwd`:/home/jovyan/work --mount type=bind,source=/data,target=/data segaleran/opencv-jupyter python /home/jovyan/work/test.py
+DIR=`pwd`
+CMD="docker run --rm -e PASSWORD=\"pass\" -v $DIR:/home/jovyan/work --mount type=bind,source=/data,target=/data segaleran/opencv-jupyter python /home/jovyan/work/test.py"
 echo $CMD
 `$CMD`
