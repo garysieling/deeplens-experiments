89c4ce53f7f4c830c3f900d2519320cf435a348c
gary <gary@garysieling.com>
2018-05-20 22:16:29 -0400

    more logging

--- training/test.sh
+++ training/test.sh
@@ -1,4 +1,8 @@
 DIR=printf "%q" `pwd`
+
+pwd
+ls /home/jovyan/work
+
 CMD="docker run --rm -e PASSWORD=\"pass\" -v $DIR:/home/jovyan/work --mount type=bind,source=/data,target=/data segaleran/opencv-jupyter python /home/jovyan/work/test.py"
 echo $CMD
 `$CMD`
