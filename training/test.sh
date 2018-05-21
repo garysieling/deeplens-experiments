f3be208097cdfcd180ce9a0f001c0138df865509
gary <gary@garysieling.com>
2018-05-21 07:52:17 -0400

    one less arg

--- training/test.sh
+++ training/test.sh
@@ -5,6 +5,6 @@ ls $DIR
 
 # /var/jenkins_home/workspace/FastTraining/training
 
-CMD="docker run --rm -e PASSWORD="pass" -it -v jenkins_home:/var/jenkins_home --mount type=bind,source=/data,target=/data segaleran/opencv-jupyter python $DIR/test.py $1"
+CMD="docker run --rm -e PASSWORD="pass" -t -v jenkins_home:/var/jenkins_home --mount type=bind,source=/data,target=/data segaleran/opencv-jupyter python $DIR/test.py $1"
 echo $CMD
 `$CMD`
