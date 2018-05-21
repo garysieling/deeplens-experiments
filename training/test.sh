d2a0e429b4c6ff5c14ef7c62b3ce63047aadc8b9
gary <gary@garysieling.com>
2018-05-20 22:30:40 -0400

    different test

--- training/test.sh
+++ training/test.sh
@@ -3,6 +3,6 @@ DIR=`pwd`
 pwd
 ls $DIR
 
-CMD="docker run --rm -e PASSWORD=\"pass\" --mount type=bind,source=$DIR,target=/work type=bind,source=/data,target=/data segaleran/opencv-jupyter ls /work"
+CMD="docker run --rm -e PASSWORD=\"pass\" --mount type=bind,source=$DIR,target=/work --mount type=bind,source=/data,target=/data segaleran/opencv-jupyter ls /data"
 echo $CMD
 `$CMD`
