9885476e338d55c0ff8f89b6e1b642f99c352294
gary <gary@garysieling.com>
2018-05-22 07:17:14 -0400

    report

--- training/report.py
+++ training/report.py
@@ -1,3 +1,7 @@
+import re
+import os
+import csv
+
 report = """                                precision    recall  f1-score   support
 
             Acadian_Flycatcher       0.00      0.00      0.00        23
@@ -5,7 +9,6 @@ report = """                                precision    recall  f1-score   supp
               Zone_tailed_Hawk       0.00      0.00      0.00        17
 
                    avg / total       0.02      0.01      0.01     11258"""
-import re
 
 report = [
   re.compile(" [ ]+").split(x.strip())
@@ -13,12 +16,11 @@ report = [
   if len(x) > 0
 ]
 
-import csv
 
 with open('test.csv', 'w') as csvfile:
   csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
   csvwriter.writerow(['build number', 'label', 'precision', 'recall', 'f1-score', 'support'])
 
   [
-    csvwriter.writerow(['1'] + x) for x in report
+    csvwriter.writerow([os.environ['BUILD_NUMBER']] + x) for x in report
   ]
