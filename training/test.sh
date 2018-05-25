3ed704b8fb7608cb1581a64215204a7fd5ecd4ec
gary <gary@garysieling.com>
2018-05-25 10:41:15 -0400

    update build script

--- training/test.sh
+++ training/test.sh
@@ -1,3 +1,5 @@
+printenv 
+
 cd training
 chmod +x test.sh
 DIR=`pwd`
@@ -5,4 +7,4 @@ DIR=`pwd`
 pwd
 ls $DIR
 
-docker run --rm -e PASSWORD="pass" -t -v jenkins_home:/var/jenkins_home --mount type=bind,source=/data,target=/data segaleran/opencv-jupyter python $DIR/test.py 4
+docker run --rm -e PASSWORD="pass" -t -v jenkins_home:/var/jenkins_home --mount type=bind,source=/data,target=/data -e "BUILD_NUMBER=$BUILD_NUMBER" -e "BUILD_NAME=$JOB_NAME" segaleran/opencv-jupyter python $DIR/test.py 4
